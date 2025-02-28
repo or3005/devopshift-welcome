0010d65e8cbb240bb3ab52dde4e77d3adc8a911c
ORB <orbiton3005@gmail.com>
2025-02-28 16:51:57 +0200

    somehomework

---
+++ labs/lab5.groovy
@@ -0,0 +1,64 @@
+node {
+
+
+  def runStage = { stageName, command ->
+        stage(stageName) {
+            echo "Starting stage: ${stageName}"
+            sh command
+        }
+    }
+
+
+    stage('Clone'){
+        runStage('Clone',  echo 'cloning repostry...')
+       
+    }
+
+    stage('Build'){
+        runStage('Buiild', echo 'building python project...')
+       
+    }
+
+
+    parallel{
+        
+        stage('Lint'){
+            runStage('Lint', echo "Running Pyhthon linting")
+            
+        }
+
+        stage('Security test'){
+            runStage('Security test', echo "Running security checks (e.g, safety)")
+        }
+
+    }
+
+  
+
+    stage('Deploy'){
+
+          steps {
+            script {
+                def userInput = input message: 'Do you want to proceed with deployment?', 
+                                     parameters: [choice(name: 'Approve', choices: 'Proceed\nAbort', description: 'Select Proceed to deploy, Abort to cancel.')]
+
+                if (userInput == 'Proceed') {
+                    runStage('Deploy', echo "Deploying Python project to production...")
+                    
+
+                } else {
+                    error 'Deployment aborted by user!'
+                }
+            }
+        }
+    }
+
+
+
+    stage('Cleanup') {
+    runStage('Cleanup', 'echo "Cleaning up workspace..."')
+    }
+}
+
+
+
